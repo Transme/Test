2825d04d62dc1d877fe042def7f4a95ab53bd080
Luoyw <1505872625@qq.com>
2019-11-12 14:07:02 +0800

    add listener and filter

--- src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-    public static int add(int a, int b){
-        return a + b;
-    }
-}
